6b0710ab6d0c07a5df0dfaa40df8a4657595a495
Tzu-Tang Hsu <vichsu416@gmail.com>
2017-06-08 10:03:58 -0400

    final

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ $(document).ready(function(){
     var express = $(this).html(); 
     console.log(express);
     
-    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + express + "&api_key=dc6zaTOxFJmzC";
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + express + "&api_key=dc6zaTOxFJmzC";
         console.log(queryURL);
         $.ajax({url: queryURL, method: 'GET'})
         .done(function(response) {
